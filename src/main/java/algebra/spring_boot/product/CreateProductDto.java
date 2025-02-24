bf38b4ebb74cf0f03b06fd96734fe35153f6467a
David Strugacevac <dstrugacevac@layer.agency>
2025-02-24 20:17:44 +0100

    Add validation

--- src/main/java/algebra/spring_boot/product/CreateProductDto.java
+++ src/main/java/algebra/spring_boot/product/CreateProductDto.java
@@ -1,12 +1,26 @@
 package algebra.spring_boot.product;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import java.util.List;
+
 
 @Data
 @AllArgsConstructor
 public class CreateProductDto {
 
+    @NotBlank
+    @Size(min = 3, max = 13)
     private String name;
 
+
+    @PositiveOrZero
+    private Integer quantity;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 1, max = 2)
+    private List<Integer> grades;
 }
--- src/main/java/algebra/spring_boot/product/ProductController.java
+++ src/main/java/algebra/spring_boot/product/ProductController.java
@@ -1,7 +1,11 @@
 package algebra.spring_boot.product;
 
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,27 +18,32 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping("/{id}")
-    public Product findById(@PathVariable Long id){
-      return productService.findById(id);
+    public ResponseEntity<Product> findById(@PathVariable Long id){
+      Product product = productService.findById(id);
+      return ResponseEntity.ok(product);
     }
 
     @GetMapping()
-    public List<Product> fetchAll(){
-        return productService.fetchAll();
+    public ResponseEntity<List<Product>> fetchAll(){
+        List<Product> products= productService.fetchAll();
+        return ResponseEntity.ok(products);
     }
 
     @PostMapping()
-    public Product create(@RequestBody CreateProductDto dto){
-        return productService.create(dto);
+    public ResponseEntity<Product> create(@Valid @RequestBody CreateProductDto dto){
+        Product product = productService.create(dto);
+        return ResponseEntity.status(201).body(product);
     }
 
     @PutMapping("/{id}")
-    public Product update(@RequestBody UpdateProductDto dto, @PathVariable Long id){
-        return productService.update(id, dto);
+    public ResponseEntity<Product> update(@Valid @RequestBody UpdateProductDto dto, @PathVariable Long id){
+        Product product = productService.update(id, dto);
+        return ResponseEntity.status(200).body(product);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id){
+    public ResponseEntity<Void> delete(@PathVariable Long id){
         productService.delete(id);
+        return ResponseEntity.status(204).build();
     }
 }
